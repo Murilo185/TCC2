d2e513a39e6af2a178ebe8c830faf4c62558739a
Murilo Mendes Pedra <85187787+Murilo185@users.noreply.github.com>
2024-09-07 21:17:23 -0300

    correção da camisa e da imagem do usuario

--- src/components/PersonalizarProduto.jsx
+++ src/components/PersonalizarProduto.jsx
@@ -250,35 +250,35 @@ export default function PersonalizarProduto() {
       <div className="bg-[#999999] w-full md:w-[50%] mx-auto rounded-[10px] mb-2">
         <div className="flex items-center justify-center">
           <div className='px-2 py-2'>
-            <Dropdown>
-              <Dropdown.Toggle variant="success" id="dropdown-basic" >
-                <p>Sua estampa</p>
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                {!imagemSelecionada && (
-                  <button
-                    onClick={() => inputFileRef.current.click()}
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                  >
-                    Enviar sua imagem aqui
-                  </button>
-                )}
-                {imagemSelecionada && (
-                  <img
-                    src={imagemSelecionada}
-                    alt=""
-                    className="w-16 h-16 object-cover cursor-pointer"
-                    onClick={() => inputFileRef.current.click()}
-                  />
-                )}
-                <input
-                  type="file"
-                  ref={inputFileRef}
-                  style={{ display: 'none' }}
-                  onChange={handleImageUpload}
-                />
-              </Dropdown.Menu>
-            </Dropdown>
+          <Dropdown>
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+    <p>Sua estampa</p>
+  </Dropdown.Toggle>
+  <Dropdown.Menu>
+    {!imagemSelecionada && !estampaSelecionada && (
+      <button
+        onClick={() => inputFileRef.current.click()}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Enviar sua imagem aqui
+      </button>
+    )}
+    {imagemSelecionada && (
+      <button
+      onClick={() => inputFileRef.current.click()}
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Enviar sua imagem aqui
+    </button>
+    )}
+    <input
+      type="file"
+      ref={inputFileRef}
+      style={{ display: 'none' }}
+      onChange={handleImageUpload}
+    />
+  </Dropdown.Menu>
+</Dropdown>
           </div>
           <div>
             <div>
